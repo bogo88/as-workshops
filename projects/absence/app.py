61f912fc485bd8d7d8970faa9ca1ba6e34fea2b4
Boguslaw Szczuka <you@example.com>
2015-02-10 17:04:34 +0100

    connect to MongoDB

--- projects/absence/app.py
+++ projects/absence/app.py
@@ -1,77 +1,64 @@
-from contextlib import closing
-import sqlite3
-from flask import Flask, request, session, g, redirect, url_for, \
-    abort, render_template, flash
-
-# configuration
-DATABASE = '/tmp/app.db'
-DEBUG = True
-SECRET_KEY = 'development key'
-USERNAME = 'admin'
-PASSWORD = 'default'
+import json
+import isodate
+from flask import Flask
+from pymongo import MongoClient
+from urllib3 import request
+from bson import json_util
 
 app = Flask(__name__)
-app.config.from_object(__name__)
-app.config.from_envvar('APP_SETTINGS', silent=True)
+
+client = MongoClient()
+db = client.as_workshops
 
 
-def init_db():
-    with closing(connect_db()) as db:
-        with app.open_resource('schema.sql', mode='r') as f:
-            db.cursor().executescript(f.read())
-        db.commit()
-
-
-def connect_db():
-    return sqlite3.connect(app.config['DATABASE'])
-
-
-@app.before_request
-def before_request():
-    g.db = connect_db()
-
-
-@app.teardown_request
-def teardown_request(exception):
-    db = getattr(g, 'db', None)
-    if db is not None:
-        db.close()
-
+def to_json(data):
+    """Convert Mongo object(s) to JSON"""
+    return json.dumps(data, default=json_util.default)
 
 @app.route('/absence', methods=['get'])
 def absence_list():
-    cur = g.db.execute('select * from absence')
-    absence = cur.fetchall()
-    return str(absence)
+    results = db['absences'].find()
+    json_results = []
+    for result in results:
+        json_results.append(result)
+    return to_json(json_results)
 
 
-@app.route('/absence/<int:absence_id>', methods=['get'])
-def absence_detail(absence_id):
-    return 'Absence detail {}'.format(absence_id)
+@app.route('/absence/date/<absence_date>', methods=['get'])
+def absence_detail(absence_date):
+    results = db['absences'].find({'absence_start': {'$lte': absence_date}, 'absence_end': {'$gte': absence_date}})
+    json_results = []
+    for result in results:
+        json_results.append(result)
+    return to_json(json_results)
 
 
 @app.route('/absence', methods=['post'])
 def absence_create():
-    g.db.execute(
-        'insert into absence (user_id, absence_status, absence_type, absence_start, absence_end) values (?, ?, ?, ?, ?)',
-        [requests.post['user_id'],
-         requests.post['absence_status'],
-         requests.post['absence_type'],
-         requests.post['absence_start'],
-         requests.post['absence_end']]
-    )
-    g.db.commit()
-    return 'Absence create'
+    record = {
+        'name': "TestName",
+        'surname': "TestSurname",
+        'absence_start': "2015-01-02",
+        'absence_end': "2015-01-09",
+        #'name': request.POST['name'],
+        #'surname': request.POST['surname'],
+        #'absence_start': request.POST['absence_start'],
+        #'absence_end': request.POST['absence_end']
+    }
+    absences = db.absence
+    absence_id = absences.insert(record)
+    return 'Absence {} created\n'.format(absence_id)
+    #return 'Absence  created\n'
 
 
 @app.route('/absence/<int:absence_id>', methods=['put'])
 def absence_edit(absence_id):
-    return 'Absence edit {}'.format(absence_id)
+    return 'Absence edit {}\n'.format(absence_id)
 
 
 @app.route('/absence/<int:absence_id>', methods=['delete'])
 def absence_delete(absence_id):
-    return 'Absence delete {}'.format(absence_id)
+    return 'Absence delete {}\n'.format(absence_id)
 
 
 if __name__ == '__main__':
