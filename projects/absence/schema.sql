7fea8c95e82f23c9823f394886a452e2d62ea780
Boguslaw Szczuka <you@example.com>
2015-02-10 17:11:21 +0100

    remove schema

--- projects/absence/schema.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists absence;
-create table absence (
-  id integer primary key autoincrement,
-  user_id integer not null,
-  absence_status integer,
-  absence_type integer,
-  absence_start date,
-  absence_end date
-);
