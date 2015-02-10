43bfa23b0f8073b7b51305bfcd024ad1df502850
Boguslaw Szczuka <you@example.com>
2015-02-10 17:10:51 +0100

    remove model

--- projects/absence/models.py
+++
@@ -1,8 +0,0 @@
-from app import db
-
-
-class Create(db.Document):
-    name = db.StringField(max_length=255, required=True)
-    surname = db.StringField(max_length=255, required=True)
-    absence_start = db.DateTimeField(required=True)
-    absence_end = db.DateTimeField(required=True)
