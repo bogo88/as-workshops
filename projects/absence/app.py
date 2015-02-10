7f4f7da829d9e9cc3c98352e299940570416031a
Boguslaw Szczuka <you@example.com>
2015-02-10 17:35:38 +0100

    a

--- projects/absence/app.py
+++ projects/absence/app.py
@@ -1,8 +1,6 @@
 import json
-import isodate
 from flask import Flask
 from pymongo import MongoClient
-from urllib3 import request
 from bson import json_util
 
 app = Flask(__name__)
@@ -16,7 +14,7 @@ def to_json(data):
     return json.dumps(data, default=json_util.default)
 
 @app.route('/absence', methods=['get'])
-def absence_list():
+def absence_list(): 
     results = db['absences'].find()
     json_results = []
     for result in results:
@@ -35,20 +33,7 @@ def absence_detail(absence_date):
 
 @app.route('/absence', methods=['post'])
 def absence_create():
-    record = {
-        'name': "TestName",
-        'surname': "TestSurname",
-        'absence_start': "2015-01-02",
-        'absence_end': "2015-01-09",
-        #'name': request.POST['name'],
-        #'surname': request.POST['surname'],
-        #'absence_start': request.POST['absence_start'],
-        #'absence_end': request.POST['absence_end']
-    }
-    absences = db.absence
-    absence_id = absences.insert(record)
-    return 'Absence {} created\n'.format(absence_id)
-    #return 'Absence  created\n'
+    return 'Absence  created\n'
 
 
 @app.route('/absence/<int:absence_id>', methods=['put'])
